b83ac4e6dc3999cefe43bbb1a3d5b56ea93c2451
DarshanR <darshanramesh6900@gmail.com>
2025-08-25 03:04:00 +0530

    Trying to deploy Frontend

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FiPlus, FiMinus } from "react-icons/fi";
 import { Link, useLocation } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
 import prod from "../../public/assets/prods.jpg";
 
 export default function Product() {
