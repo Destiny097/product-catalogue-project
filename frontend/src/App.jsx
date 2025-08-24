06def62f02ccf0af424ac13d4eb48ce144289cab
DarshanR <darshanramesh6900@gmail.com>
2025-08-25 03:00:18 +0530

    Trying to deploy Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Hero from './pages/Hero';
 import BestSellers from './pages/BestSellers';
