e2ce2c491800ac5d4c4771dc80f888aac5de2ac3
DarshanR <darshanramesh6900@gmail.com>
2025-08-25 10:05:09 +0530

    Fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,36 @@
-import express from  "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import authRoutes from "./routes/auth.js";
 import productRoutes from "./routes/productRoutes.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/auth",authRoutes);
-app.use("/api/products",productRoutes);
-app.use("/api/collections",collectionRoutes);
-app.use("/api/cart",cartRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/orders",orderRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log("MongoDB connected")).catch(err=> console.log(err));
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/collections", collectionRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("API is Running");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
 });
 
-const port =process.env.PORT;
-app.listen(port,()=>console.log(`Server running on ${port}`));
+// ✅ Instead of app.listen, export the app for Vercel
+export default app;
