bef9c474c1da5f5113b3aac57e44565c10f41e43
DarshanR <darshanramesh6900@gmail.com>
2025-08-25 10:12:07 +0530

    Fixing bugs

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,6 +4,6 @@ const API = axios.create({
     baseURL : `${import.meta.env.VITE_API_BASEURL}/api`,
 })
 
-export const getProducts = () => API.get("/products");
-export const getProductById = () => API.get(`/products/${id}`);
-export const getCollections = () => API.get(`/collections`);
+export const getProducts = () => API.get("/api/products");
+export const getProductById = () => API.get(`/api/products/${id}`);
+export const getCollections = () => API.get(`/api/collections`);
